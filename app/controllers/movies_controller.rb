99c6ab4cb813eb5948b2e2c77832bf94621da3bb
KEITH NOLAN <g00351932@gmit.ie>
2021-03-03 12:01:17 +0000

    Got front end working

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,9 +7,46 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+    # All movie ratings
+    @all_ratings = Movie.all_ratings
+    #Show selected ratings
+    if params[:ratings]
+      @ratings_to_show = params[:ratings]
+    elsif session[:ratings]
+      @ratings_to_show = session[:ratings]
+    else
+      #If no selection show all
+      @ratings_to_show = Hash[@all_ratings.map {|key| [key, 1]}]
   end
-
+    #https://www.rubyguides.com/2019/06/rails-params/
+    #Sorting paramters
+    if params[:order]
+      sorted_params = params[:order]
+    elsif session[:order]
+      sorted_params = session[:order]
+    end
+    
+    if sorted_params
+      #If sorted by title show in acesending order
+      if sorted_params == 'title'
+        ordered, @title_header = {:title => :asc}, "hilite bg-warning"
+      #If sorted by release_date show in acesending order
+      elsif sorted_params == 'release_date'
+        ordered, @release_date_header = {:release_date => :asc}, "hilite bg-warning"
+      end
+    end
+    
+    if (params[:ratings] ==nil && session[:order] != nil) ||
+        (params[:order] ==nil && session[:order] != nil)
+      redirect_to movies_path :order => sorted_params, :ratings => @ratings_to_show and return
+    end
+    
+    @movies = Movie.where(rating: @ratings_to_show.keys).order(ordered)
+    session[:order] = sorted_params
+    session[:ratings] = @ratings_to_show
+    
+  end
+   
   def new
     # default: render 'new' template
   end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,16 @@
 class Movie < ActiveRecord::Base
+  
+  def self.all_ratings
+    ['G','PG', 'PG-13', 'R']
+  end
+  def self.with_ratings(ratings_list)
+  # if ratings_list is an array such as ['G', 'PG', 'R'], retrieve all
+  if ratings_list
+    #  movies with those ratings
+    Movie.where(rating: ratings_list.k)
+  # if ratings_list is nil, retrieve ALL movies
+  else
+    Movie.all
+  end
+ end
 end
